45697d78491c0e7b7118a2be7456eae7ada4ef6d
LieneLittlebird <liene.putnina20@gmail.com>
2021-03-01 22:35:44 +0200

    program initialized and first print done

---
+++ labA.py
@@ -0,0 +1,46 @@
+# Liene Putniņa, lr12022
+# A4. Given n integers. Calculate the minimum value of given integers and the number of the integers with this value.
+# Program created at: 2021/03/01
+
+#VALIDATION
+#ADD TO TEST PLAN
+
+def findNumbers ():
+    numbers = []
+    # numbers.sort()
+    count = int
+    minValue = int
+    continueLoop = bool
+
+    count = input("Enter the number of elements in an array:\n")
+
+    print(f"Enter {count} numbers: \n")
+
+    
+    # for i in range(len(numbers)):
+    #         print(numbers[i])
+    
+    # minValue = numbers[0]
+    # result = 0;
+
+    # for i in range(len(numbers)):
+    #     if numbers[i] < minValue:
+    #         minValue = numbers[i];
+    #         result = 1;
+    #     elif minValue == numbers[i]:
+    #         result += 1
+    
+    # print(f"The smallest number is: {minValue}. It occurs {result} times")
+
+    return 0;
+
+findNumbers()
+
+    # repeatExec = int;
+
+
+    #do findNumbers()
+    #print("Enter 1 to continue or 0 to quit:")
+    # val input Enter 1 to continue or 0 to quit
+    #print (val)
+    # while repeatExec = 1
